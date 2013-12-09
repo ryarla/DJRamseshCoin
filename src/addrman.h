0e9c61ebe0c6daaea4b0b7487a2b733482cb1d7c
ryarla <ryarla@gmail.com>
2013-12-09 10:55:00 -0500

    Update addrman.h

--- src/addrman.h
+++ src/addrman.h
@@ -2,7 +2,7 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file license.txt or http://www.opensource.org/licenses/mit-license.php.
 #ifndef _BITCOIN_ADDRMAN
-#define _BITCOIN_ADDRMAN 1
+#define _BITCOIN_ADDRMAN 1 
 
 #include "netbase.h"
 #include "protocol.h"
